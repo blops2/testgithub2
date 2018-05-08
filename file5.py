b4b39d0eee0e3de4cd5575a0911982bf7dfbbdd5
David Thomson <dmt257257@gmail.com>
2018-05-08 12:08:04 +0100

    added file5.py

---
+++ file5.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+"""
+"""
+
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s - %(levelname)s - %(message)s")
+#logging.disable(logging.CRITICAL)
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
